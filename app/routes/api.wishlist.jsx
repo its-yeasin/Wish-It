59f45fe2c792b026fb22929b8424f6eda9ea9cc9
Yeasin Arafat <yeasinn2002@gmail.com>
2025-03-03 21:02:03 +0600

    api method testes

--- app/routes/api.wishlist.jsx
+++ app/routes/api.wishlist.jsx
@@ -1,6 +1,21 @@
-export const loader = async () => {
-  return Response.json({
-    name: "My Store",
-    description: "My Store Description",
-  });
+export const loader = async ({ request }) => {
+  return Response.json({ message: "Hello World", method: request.method });
+};
+
+export const action = async ({ request }) => {
+  switch (request.method) {
+    case "GET":
+      return Response.json({ message: "Hello World", method: request.method });
+    case "POST":
+      return Response.json({ message: "Hello World", method: request.method });
+    case "PUT":
+      return Response.json({ message: "Hello World", method: request.method });
+    case "PATCH":
+      return Response.json({ message: "Hello World", method: request.method });
+    case "DELETE":
+      return Response.json({ message: "Hello World", method: request.method });
+
+    default:
+      return Response.json({ message: "Hello World", method: "DEFAULT" });
+  }
 };
